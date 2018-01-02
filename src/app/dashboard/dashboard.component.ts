094b9637c709954774055c237c393bd57aad4d19
omar olvera <omar.rodriguez@mexmax-internet.com>
2018-01-02 17:08:57 -0600

    added firebase hosting files

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
  
 })
 export class DashboardComponent implements OnInit {
-name:string= "Pedro"
+name = 'Omar';
   constructor() { }
 
   ngOnInit() {
